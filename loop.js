fdc18c5dfa3dd037efa43cd2f5a181ff6e5072af
evently58-a11y <evently58@gmail.com>
2026-08-25 15:28:41 +0100

    This is where i joined the class seriously because the previous classes has been master before

---
+++ loop.js
@@ -0,0 +1,12 @@
+for(let x =1; x<=12; x++){
+    console.log(x*3)
+}
+
+
+
+let score =[20,12,25,15]
+
+for (let studentScore =0;studentScore <score.length; studentScore++){
+    console.log(score[studentScore]+20)
+
+}
